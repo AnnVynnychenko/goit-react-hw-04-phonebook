86f55d1a5c1ebad98f52428b3d1a8c7f0b5771f4
AnnVynnychenkoa <avinnichenko93@gmail.com>
2023-08-10 14:20:39 +0300

    hw-04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Phonebook from './Phonebook';
+import Contacts from './Contacts';
+
+function App() {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = contactData => {
+    console.log('contactData', contactData);
+
+    const { name } = contactData;
+    const ifNameTaken = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (ifNameTaken) {
+      return alert(`${name} is already in contacts`);
+    }
+    const contactDataWithId = { ...contactData, id: nanoid() };
+    setContacts(prevContacts => [...prevContacts, contactDataWithId]);
+  };
+
+  const deleteContact = contactId => {
+    const filteredContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    setContacts(filteredContacts);
+  };
+
+  const filterContacts = e => {
+    const { value } = e.currentTarget;
+    setFilter(value);
+  };
+
+  const visibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(
+      contact =>
+        contact.name && contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Phonebook titleBegin="Phone" titleEnd="book">
+      <ContactForm onSubmit={formSubmitHandler} />
+      <Contacts title="Contacts">
+        <Filter filterValue={filter} onFilterContacts={filterContacts} />
+        <ContactList
+          contacts={visibleContacts()}
+          onDeleteContact={deleteContact}
+        />
+      </Contacts>
+    </Phonebook>
   );
-};
+}
+
+export default App;
